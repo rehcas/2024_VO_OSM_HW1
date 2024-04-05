9fe1643b2cb1ef95b8314697e6e2b195dc1cfce2
Christoph Sacher <christoph.sacher@gmail.com>
2024-04-05 12:32:48 +0200

    Updated 'calc.py'
    added pep8 incompatibality

--- calc.py
+++ calc.py
@@ -1,18 +1,19 @@
 #Add
 def add(a, b):
-	return a + b
+	return a +b
 #Substract
 def sub(a, b):
 	return a - b
 #Multiply
 def multi(a,b):
+
 	return a * b
 #Divide
 def div(a, b):
 	return a / b
 #Modulo
 def mod(a, b):
-	return a % b
+	return a% b
 		
 
 	
