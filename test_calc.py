765d13c0a333e7edd57fc57f2e61d12fd39e7889
JoanaArapi <163778563+JoanaArapi@users.noreply.github.com>
2024-04-09 15:29:05 +0200

    make test_calc.py fail

--- test_calc.py
+++ test_calc.py
@@ -3,8 +3,8 @@ from calc import add, sub, multi, div, mod
 
 # Test add()
 def test_add():
-    assert add(1, 1) == 2
-    assert add(10, 10) == 20
+    assert add(1, 1) == 3
+    assert add(10, 10) == 10
     assert add(10, -1) == 9
     assert add(100, 100) == 200
 
