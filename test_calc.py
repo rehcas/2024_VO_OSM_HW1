33cd95b595fc6c53a1a02e207b3e0b10f229f05e
JoanaArapi <163778563+JoanaArapi@users.noreply.github.com>
2024-04-09 15:36:27 +0200

    Update test_calc.py works now

--- test_calc.py
+++ test_calc.py
@@ -3,15 +3,15 @@ from calc import add, sub, multi, div, mod
 
 # Test add()
 def test_add():
-    assert add(1, 1) ==3
-    assert add(10, 10) ==10
+    assert add(1, 1) == 2
+    assert add(10, 10) == 20
     assert add(10, -1) == 9
     assert add(100, 100) == 200
 
 
 # Test sub()
 def test_sub():
-    assert sub(1, 1) == 1
+    assert sub(1, 1) == 0
     assert sub(10, 1) == 9
     assert sub(1, -1) == 2
     assert sub(100, -10) == 110
